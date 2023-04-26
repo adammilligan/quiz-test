b7be620823cfd76c7ef8845c18994c2e915be4d8
Вадим Валыгин <vadim12388@mail.ru>
2023-04-26 16:03:27 +0300

    arrow images

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './styles/radioBigStyles.css';
 import './styles/radioSmallStyles.css';
 import './styles/descriptionStyles.css';
 import './styles/selectStyles.css';
+import './styles/modalButtonsStyles.css';
 import app from './application';
 
 app();
